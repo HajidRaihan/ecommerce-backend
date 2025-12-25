f98aef7ee4daa77d8c8be3972fed49c1900ad00f
Hajid Raihan <hajidraihan@gmail.com>
2025-12-25 23:51:35 +0700

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './modules/product/product.module';
+import { PrismaModule } from './infra/database/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ProductModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
